b34f2049bb6d8756498540893e5fe8989d8b8ce0
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-06-01 16:52:27 +0900

    コメント追加

--- lambda/src/main/java/com/eazybytes/java8/fn/StringMethodWithStringParams.java
+++ lambda/src/main/java/com/eazybytes/java8/fn/StringMethodWithStringParams.java
@@ -4,7 +4,32 @@ package com.eazybytes.java8.fn;
 public interface StringMethodWithStringParams {
     public String printHello(String name);
 
+    /**
+     * defaultはOK
+     */
     public default void hi() {
         System.out.println("Hi");
+    }
+
+    /**
+     * privateメソッドはOK
+     */
+    private String someHelper() {
+        return "help you";
+    }
+
+    /**
+     * publicはNG。
+     * これはコンパイルエラーになる
+     */
+    // public void test() {
+    //   // do something
+    // }
+
+    /**
+     * staticであれば、publicもOK
+     */
+    public static String someStaticHelper() {
+        return "help you";
     }
 }
